e57549417db600d8131da68deb1770aad0a7cf4f
liyixin123 <592945148@qq.com>
2020-12-11 11:10:07 +0800

    add 1-6 exercise

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <iomanip>
+#include <vector>
+#include <array>
 void exercise1_5_string() {
   std::cout << "What's your name? ";
   std::string name;
@@ -24,8 +26,38 @@ void exercise1_5_C_style() {
 	std::cout << "Please enter two more Length \n";
   }
 }
+void ex1_6() {
+  int inputVal;
+  std::vector<int> v;
+  const int sz = 50;
+  std::array<int, 50> av{0};
+  int index = 0;
+  while (std::cin >> inputVal) {
+	v.push_back(inputVal);
+	av[index++] = inputVal;
+  }
+  int sumV = 0;
+  double avgV = 0.0;
+  for (auto val:v) {
+	sumV += val;
+  }
+  avgV = sumV / v.size();
+  std::cout << "Vector container Sum: "
+			<< sumV
+			<< " Average: " << avgV << std::endl;
+  sumV = 0;
+  avgV = 0.0;
+  for (int i = 0; i < index; i++) {
+	sumV += av[i];
+  }
+  avgV = sumV / index;
+  std::cout << "Array container Sum: "
+			<< sumV
+			<< " Average: " << avgV << std::endl;
+}
 int main() {
-  exercise1_5_string();
+//  exercise1_5_string();
 //exercise1_5_C_style();
+  ex1_6();
   return 0;
 }
